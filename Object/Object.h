a614e6930b7c83b0eecea9962995219bcd7389df
DianQK <dianqk@icloud.com>
2017-04-29 00:32:02 +0800

    Initial commit

---
+++ Object/Object.h
@@ -0,0 +1,19 @@
+//
+//  Object.h
+//  Object
+//
+//  Created by wc on 2017/4/29.
+//  Copyright © 2017年 t. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Object.
+FOUNDATION_EXPORT double ObjectVersionNumber;
+
+//! Project version string for Object.
+FOUNDATION_EXPORT const unsigned char ObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Object/PublicHeader.h>
+
+
---
+++ Object/Object.swift
@@ -0,0 +1,13 @@
+//
+//  Object.swift
+//  Object
+//
+//  Created by wc on 2017/4/29.
+//  Copyright © 2017年 t. All rights reserved.
+//
+
+import Foundation
+
+public class Object {
+    
+}
